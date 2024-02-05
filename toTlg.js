61bfa40965515ab701ddf49fb708c9977dca6631
MM07 <71704916+MM079487@users.noreply.github.com>
2024-02-06 01:56:43 +0800

    E

--- toTlg.js
+++ toTlg.js
@@ -1,20 +1,49 @@
+require("dotenv").config()
 const tlg_token = process.env.tlg_token
 
 module.exports = async (client, Discord) => {
     const fetch = require("node-fetch")
+    const chatID = -4172380255
     client.on("messageCreate", async message => {
+      const attachments = message.attachments;
+      // const url = attachment ? attachment.url : null;
       if(message.channel.id != 1203744518315184148) return true
       if(message.author.bot) return true
         // https://api.telegram.org/bot${TOKEN}
         // -4172380255
-        let txt = `__${message.author.username || "undefined" }__\n${message.content || " " }`
+        let txt = `<u>${message.author.username || "undefined" }</u>\n${message.content || " " }`
+        if(attachments.first()){
+          console.log("ATTACHMENT FOUND")
+          attachments.forEach(async att => {
+            console.log(att.url)
+            txt = `<u>${message.author.username || "undefined" }</u>\n${message.content || " " }\n<a href="${att.url}">.</a>`
+            let data = {
+              chat_id: chatID,
+              text: txt,
+              parse_mode:"html"
+            }
+            console.log(data)
+            await fetch(`https://api.telegram.org/bot${tlg_token}/sendMessage`, {
+              method:"POST",
+              headers: {"content-type": "application/json"},
+              body: JSON.stringify(data)
+            })
+            txt = ""
+            txt = `<u>${message.author.username || "undefined" }</u>\n${message.content || " " }`
+            console.log("ATTACHMENT SENT")
+          });
+          return
+        }
+        let data = {
+          chat_id: chatID,
+          text: txt,
+          parse_mode:"html"
+        }
         await fetch(`https://api.telegram.org/bot${tlg_token}/sendMessage`, {
           method:"POST",
-          body: {
-            chat_id: -4172380255,
-            text: txt
-          }
+          headers: {"content-type": "application/json"},
+          body: JSON.stringify(data)
         })
-        console.log(txt)
+
       })
     }
