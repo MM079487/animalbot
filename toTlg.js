9247f40ed2f391a07784ebc6a618091c3b9cdfc0
MM07 <71704916+MM079487@users.noreply.github.com>
2024-02-05 23:10:36 +0800

    test

--- toTlg.js
+++ toTlg.js
@@ -3,16 +3,14 @@ const tlg_token = process.env.tlg_token
 module.exports = async (client, Discord) => {
     const fetch = require("node-fetch")
     client.on("messageCreate", async message => {
-      if(message.channel.id != "1203744518315184148") return
+      if(message.channel.id != "1203744518315184148") return true
+      if(message.author.bot) return true
         // https://api.telegram.org/bot${TOKEN}
         // -4172380255
         let txt = `__${message.author.username || "undefined" }__\n${message.content || " " }`
-        await fetch(`https://api.telegram.org/bot${tlg_token}/sendMessage`, {
+        console.log(txt)
+        await fetch(`https://api.telegram.org/bot${tlg_token}/sendMessage?chat-id=${-4172380255}&text=${txt}`, {
           method:"POST",
-          data:{
-            chat_id: -4172380255,
-            text: txt
-          }  
         })
     })
 }
