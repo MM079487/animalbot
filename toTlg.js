f845fce92a66189597e6597453c744f6c9c6aea4
MM07 <71704916+MM079487@users.noreply.github.com>
2024-02-05 23:19:24 +0800

    Update toTlg.js

--- toTlg.js
+++ toTlg.js
@@ -3,7 +3,7 @@ const tlg_token = process.env.tlg_token
 module.exports = async (client, Discord) => {
     const fetch = require("node-fetch")
     client.on("messageCreate", async message => {
-      if(message.channel.id != "1203744518315184148") return true
+      if(message.channel.id != 1203744518315184148) return true
       if(message.author.bot) return true
         // https://api.telegram.org/bot${TOKEN}
         // -4172380255
