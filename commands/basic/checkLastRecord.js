ec6d798fb5d952f6b72b0854cb4a603f425aab6c
MM07 <pinshun6877@gmail.com>
2024-12-31 20:16:32 +0800

    changed checkLastRecord command footer url

--- commands/basic/checkLastRecord.js
+++ commands/basic/checkLastRecord.js
@@ -41,7 +41,7 @@ module.exports = {
 
           const embed = new EmbedBuilder()
             .setAuthor({ name: lastRecord.name, iconURL: lastRecord.authorIcon })
-            .setDescription(`\`\`\`${modifiedMsg}\`\`\`\n<t:${Math.floor(lastRecord.time / 1000)}:R> | <t:${Math.floor(lastRecord.time / 1000)}:f> - ${lastRecord.channel} (${lastRecord.guildName})\nhttps://e777eb1a-bd03-4bd2-9c5a-280c1f703ed6-00-95hbmw2y6pp8.global.replit.dev`)
+            .setDescription(`\`\`\`${modifiedMsg}\`\`\`\n<t:${Math.floor(lastRecord.time / 1000)}:R> | <t:${Math.floor(lastRecord.time / 1000)}:f> - ${lastRecord.channel} (${lastRecord.guildName})\nhttps://animal-bot-5hs7.onrender.com`)
           .setColor(0x2B2D31)
 
           interaction.editReply({ embeds: [embed] })
