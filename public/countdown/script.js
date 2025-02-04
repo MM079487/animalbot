6220be640611652134e50ffc36419e9485a8a29c
MM07 <pinshun6877@gmail.com>
2025-02-04 20:20:08 +0800

    updated spm end date

--- public/countdown/script.js
+++ public/countdown/script.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Feb 3, 2025 13:15:00").getTime();
+var countDownDate = new Date("Feb 6, 2025 17:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
