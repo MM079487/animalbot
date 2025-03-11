4e5cd86f5b870b3e123cd686a79bbb4bd3d65f39
MM07 <pinshun6877@gmail.com>
2025-03-11 21:08:32 +0800

    -Updated countdown for wei

--- public/countdown/script.js
+++ public/countdown/script.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Feb 6, 2025 17:00:00").getTime();
+var countDownDate = new Date("Aug 11, 2025 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -23,6 +23,6 @@ var x = setInterval(function() {
   // If the count down is over, write some text 
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("timer").innerHTML = "Congratulations! It's time to find a job...";
+    document.getElementById("timer").innerHTML = "阿伟 Welcome Back";
   }
 }, 100);
