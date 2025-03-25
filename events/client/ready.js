0639ded33f54be7b25be06359d8aab2528e385f6
MM07 <pinshun6877@gmail.com>
2025-03-25 15:40:59 +0800

    -Daily Countdown update (Fixed)

--- events/client/ready.js
+++ events/client/ready.js
@@ -24,7 +24,7 @@ module.exports = {
 
     loadCommands(client);
     // run autoCountdown everyday at 0:00
-    cron.schedule("* * * * *", async function () {
+    cron.schedule("0 0 * * *", async function () {
       try {
         postTime("cronjob triggered");
         autoCountdown(client)
